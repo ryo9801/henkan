a7e20aba5e860376a557480283c5a78b238fcb72
ryo9801 <166799957+ryo9801@users.noreply.github.com>
2025-05-05 21:22:28 +0900

    Add files via upload

--- henkan.py
+++ henkan.py
@@ -1,44 +1,62 @@
 def henkan_2to10(a):
-  b=0
+  d=0
   c=0
   for i in range(len(str(a)),0,-1):
-      b=2**(i-1)*(int(str(a)[i*-1]))
-      c=b+c
+      d=2**(i-1)*(int(str(a)[i*-1]))
+      c=d+c
   return c
-def henkan_10to2(l):
-  if l==0 or l==1:
-      return l
-  l1=l
-  r=0
-  l2=[]
-  l3=[]
+def henkan_2to16(a):
+  h = ["a","d","c","d","e","f"]
+  l = []
+  l1 = ""
+  d = henkan_2to10(a)
   while (1):
-    if l==0 or l==1:
+    if d < 16:
+      break
+    l.append(h[(d%16)-10] if d%16 > 9  else d%16)
+    d = d//16
+  l.append(h[(d%16)-10] if d%16 > 9  else d%16)
+  for i in range(len(l)-1,-1,-1):
+    l1 = l1 + str(l[i])
+  return l1
+def henkan_10to2(a):
+  l1 = ""
+  l = []
+  while (1):
+    if a==0 or a==1:
         break
     else:
-        l=l//2
-        r=r+1
-  for i in range(r+1):
-      l2.append(int(l1%2))
-      l1=l1//2
-  a=len(l2)
-  for j in range(a-1,-1,-1):
-      l3.append(str(l2[j]))
-  a=l3[0]
-  b=l3[1]
-  c=a+b
-  for i in range(2,len(l3)):
-      a=l3[i]
-      c=c+a
-  return int(c)
-def henkan_2to16(a):
-    h = ["A","B","C","D","E","F"]
-    l = []
-    b = henkan_2to10(a)
-    if b%16 >= 10 :
-        l.append(h[b%16-10])
-    while (1):
-      if b < 16:
-          break
-    
-    return l
+      l.append(a%2)
+      a=a//2  
+  l.append(a)
+  for i in range(len(l)-1,-1,-1):
+    l1 = l1 + str(l[i])
+  return l1
+def henkan_10to16(a):
+  h = ["a","d","c","d","e","f"]
+  l = []
+  l1 = ""
+  while (1):
+    if a < 16:
+      break
+    l.append(h[(a%16)-10] if a%16 > 9  else a%16)
+    a = a//16
+  l.append(h[(a%16)-10] if a%16 > 9  else a%16)
+  for i in range(len(l)-1,-1,-1):
+    l1 = l1 + str(l[i])
+  return l1
+def henkan_16to2(a):
+  d=0
+  c=0
+  for i in range(len(str(a)),0,-1):
+      d=16**(i-1)*(int(str(a)[i*-1]))
+      c=d+c
+  c =henkan_10to2(c)
+  return c
+def henkan_16to10(a):
+  d=0
+  c=0
+  for i in range(len(str(a)),0,-1):
+      d=16**(i-1)*(int(str(a)[i*-1]))
+      c=d+c
+  return c
